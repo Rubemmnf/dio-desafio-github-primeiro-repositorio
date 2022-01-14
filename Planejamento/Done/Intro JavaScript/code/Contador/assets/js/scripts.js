4f3fb289add8776209d709c0a8bd0d12ac7c76ca
Rubem Novellino <86675691+Rubemmnf@users.noreply.github.com>
2022-01-14 18:10:50 -0300

    Fim intro js

---
+++ Planejamento/Done/Intro JavaScript/code/Contador/assets/js/scripts.js
@@ -0,0 +1,18 @@
+var currentNumberWrapper = document.getElementById("currentNumber");
+var currentNumber = 0
+
+function increment () {
+    if (currentNumber < 10) {
+        currentNumber = currentNumber + 1;
+        currentNumberWrapper.innerHTML = currentNumber;
+    }
+    else {
+        currentNumberWrapper.innerHTML = currentNumber;
+    }
+    
+}
+
+function decrement () {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
